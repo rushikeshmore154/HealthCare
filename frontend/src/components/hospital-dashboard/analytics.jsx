d78adf8b0bc393be9a45ba7a465822784d77fbed
Harshal-3354 <harshalkolhe0908@gmail.com>
2025-09-11 22:59:16 +0530

    Updated ml model link to cloud link of ml model

--- frontend/src/components/hospital-dashboard/analytics.jsx
+++ frontend/src/components/hospital-dashboard/analytics.jsx
@@ -17,7 +17,7 @@ export default function Analytics() {
 
     try {
       const res = await axios.get(
-        `http://localhost:4000/api/hospital/predict-patients?date=${selectedDate}`
+        `https://healhcare-app-2.onrender.com/api/hospital/predict-patients?date=${selectedDate}`
       );
 
       const data = res.data;
